be9986738b5daa248b94a11ef13c4bd3999bba47
Saurabh Chaturvedi <saurabh.chaturvedi63@gmail.com>
2019-06-15 06:20:30 +0530

    Slight fix in model parsing

--- XData/testDataGen/PopulateTestData.java
+++ XData/testDataGen/PopulateTestData.java
@@ -198,27 +198,32 @@ public class PopulateTestData {
 			File testFile = new File(Configuration.homeDir+"/temp_smt"+filePath+"/cut_" + cvcOutputFileName);
 			BufferedReader input =  new BufferedReader(new FileReader(Configuration.homeDir+"/temp_smt"+filePath+"/" + cvcOutputFileName));
 			try {
-				String line = null; 
-				while (( line = input.readLine()) != null){
-					if(line.contains("(define-fun ")  && line.contains("_TupleType")){
-						//setContents(testFile,line, true);
-						while ((line = input.readLine()) != null && !line.contains("))")) {
-							if(!line.contains("(ite") && !line.contains("!")) {
-									if(line.contains(")"))
-										setContents(testFile,line+"\n", true);
-									else
-										setContents(testFile,line, true);
-							}		
-						}
-						if(!line.contains("!"))
-						setContents(testFile,line+"\n", true);
-					}
-					//Application Testing - to include value of program parameters
-					if(line.startsWith("ASSERT (parameter")){
-						setContents(testFile, line+"\n", true);
-					}
-				}
-			}catch(Exception e){
+                String line = null; 
+                while ((line = input.readLine()) != null) {
+                    if(line.contains("(define-fun ")  && line.contains("_TupleType")) {
+                        //setContents(testFile,line, true);
+                        while ((line = input.readLine()) != null) {
+                            if(!line.contains("(ite") && !line.contains("!")) {
+                                    if(line.contains("define-fun")) {
+                                        //input.mark(1000);
+                                        input.reset();
+                                        break;
+                                    } else if (line.startsWith(")")) {
+                                        break;
+                                    }
+                                    setContents(testFile,line+"\n", true);
+                            }
+                            input.mark(100);
+                        }
+                        //if(!line.contains("!"))
+                        //setContents(testFile,line+"\n", true);
+                    }
+                    //Application Testing - to include value of program parameters
+                    if(line.startsWith("ASSERT (parameter")){
+                        setContents(testFile, line+"\n", true);
+                    }
+                }
+            } catch (Exception e) {
 				logger.log(Level.SEVERE,"PopulateTestData-cutRequiredOutput :  "+e.getStackTrace(),e);
 			}
 			finally {
